88bb0fabc55a7a4a41d3385d77ad1c6f88b738c8
sinlov <sinlovgmppt@gmail.com>
2016-09-05 23:56:14 +0800

    add new test of hex to bytes

--- TestForm/src/main/java/com/sinlov/my/test/HexUtils.java
+++ TestForm/src/main/java/com/sinlov/my/test/HexUtils.java
@@ -33,6 +33,32 @@ public class HexUtils {
         return result;
     }
 
+    public static byte[] hexStringToBytes(String hexString) {
+        if (hexString == null || hexString.equals("")) {
+            return null;
+        }
+        hexString = hexString.toUpperCase();
+        int length = hexString.length() / 2;
+        char[] hexChars = hexString.toCharArray();
+        byte[] d = new byte[length];
+        for (int i = 0; i < length; i++) {
+            int pos = i * 2;
+            byte h = charToByte(hexChars[pos]);
+            byte l = charToByte(hexChars[pos + 1]);
+            d[i] = (byte) (h << 4 | l);
+        }
+        return d;
+    }
+
+    /**
+     * Convert char to byte
+     * @param c char
+     * @return byte
+     */
+    private static byte charToByte(char c) {
+        return (byte) "0123456789ABCDEF".indexOf(c);
+    }
+
 
     public static String bytes2HexStr(byte[] datas) {
         if (datas == null || datas.length == 0) {
--- TestForm/src/test/java/com/sinlov/my/test/TestHexUtils.java
+++ TestForm/src/test/java/com/sinlov/my/test/TestHexUtils.java
@@ -24,15 +24,30 @@ import org.junit.Test;
  */
 public class TestHexUtils {
 
-    public static final String TEST_STR = "*f*#&&11(g*Uj!hg";
+    public static final String TEST_STR_TWO = "12345678qwertyui";
+    public static final String TEST_HEX_STR = "4fb136d957b092cd29";
 
     @Test
     public void testHexStr2Bytes() {
-        byte[] datas = TEST_STR.getBytes();
-        System.out.println("Datas: " + HexUtils.bytes2HexStr(datas));
+        System.out.println("hex string: " + (byte) (79));
+        System.out.println("hex string: " + (byte) (177));
+        System.out.println("hex string: " + (byte) (0x75));
+        System.out.println("hex string: " + (byte) (177 & 0xffff));
+        byte[] bytes = HexUtils.hexStringToBytes("4fb136d957b092cd29c9b8555167b730976596f12af1ba99032f87ee753e2d465105e92919e81f6ed2de0af9abb98ca08d5db6fe46085a0b1158a5a7d349feea");
+        for (byte d :
+                bytes) {
+            System.out.println("hexStringToBytes: " + HexUtils.byte2HexStr(d));
+        }
+        byte[] hexBytes = HexUtils.hexStr2Bytes(TEST_HEX_STR);
+        for (byte b :
+                hexBytes) {
+            System.out.println("hexBytes to byte: " + HexUtils.byte2HexStr(b));
+        }
+        byte[] strDatas = TEST_STR_TWO.getBytes();
+        System.out.println("Datas: " + HexUtils.bytes2HexStr(strDatas));
         for (byte data :
-                datas) {
-            System.out.println("str to bytes: " + HexUtils.byte2HexStr(data));
+                strDatas) {
+            System.out.println("str to byte: " + HexUtils.byte2HexStr(data));
         }
     }
 }
