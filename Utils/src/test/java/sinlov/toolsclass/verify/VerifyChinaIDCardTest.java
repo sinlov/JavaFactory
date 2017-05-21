15101449ff9fbace57e7868f4715b6cfa5d90d3d
sinlov <sinlovgmppt@gmail.com>
2017-05-22 00:24:26 +0800

    use new temple at unit test of code

--- Utils/src/test/java/sinlov/toolsclass/verify/VerifyChinaIDCardTest.java
+++ Utils/src/test/java/sinlov/toolsclass/verify/VerifyChinaIDCardTest.java
@@ -1,4 +1,4 @@
-package sinlov.toolsclass.verify;
+package sinlov.toolsclass.verify; 
 
 import org.junit.Test; 
 import org.junit.Before; 
@@ -7,109 +7,109 @@ import org.junit.After;
 /** 
 * VerifyChinaIDCard Tester. 
 * 
-* @author <Authors name> 
-* @since <pre>五月 19, 2017</pre> 
+* @author sinlov
+* @since <pre>05/22/2017</pre> 
 * @version 1.0 
 */ 
-public class VerifyChinaIDCardTest { 
+public class VerifyChinaIDCardTest {
 
-@Before
-public void before() throws Exception { 
-} 
+    @Before
+    public void before() throws Exception { 
+    } 
 
-@After
-public void after() throws Exception { 
-} 
+    @After
+    public void after() throws Exception { 
+    } 
 
-/** 
-* 
-* Method: getInstance() 
-* 
-*/ 
-@Test
-public void testGetInstance() throws Exception { 
-//TODO: Test goes here... 
-} 
+    /** 
+     * 
+     * Method: getInstance() 
+     * 
+     */ 
+    @Test
+    public void testGetInstance() throws Exception { 
+        //TODO: Test goes here... 
+    } 
 
-/** 
-* 
-* Method: authentication(String iDStr) 
-* 
-*/ 
-@Test
-public void testAuthentication() throws Exception { 
-//TODO: Test goes here... 
-} 
+    /** 
+     * 
+     * Method: authentication(String iDStr) 
+     * 
+     */ 
+    @Test
+    public void testAuthentication() throws Exception { 
+        //TODO: Test goes here... 
+    } 
 
-/** 
-* 
-* Method: getAreaCodeFull() 
-* 
-*/ 
-@Test
-public void testGetAreaCodeFull() throws Exception { 
-//TODO: Test goes here... 
-} 
+    /** 
+     * 
+     * Method: getAreaCodeFull() 
+     * 
+     */ 
+    @Test
+    public void testGetAreaCodeFull() throws Exception { 
+        //TODO: Test goes here... 
+    } 
 
 
-/** 
-* 
-* Method: checkSortIDYearCode(String iDStr) 
-* 
-*/ 
-@Test
-public void testCheckSortIDYearCode() throws Exception { 
-//TODO: Test goes here... 
-/* 
-try { 
-   Method method = VerifyChinaIDCard.getClass().getMethod("checkSortIDYearCode", String.class); 
-   method.setAccessible(true); 
-   method.invoke(<Object>, <Parameters>); 
-} catch(NoSuchMethodException e) { 
-} catch(IllegalAccessException e) { 
-} catch(InvocationTargetException e) { 
-} 
-*/ 
-} 
+    /** 
+     * 
+     * Method: checkSortIDYearCode(String iDStr) 
+     * 
+     */ 
+    @Test
+    public void testCheckSortIDYearCode() throws Exception { 
+        //TODO: Test goes here... 
+        /*
+        try {
+           Method method = VerifyChinaIDCard.getClass().getMethod("checkSortIDYearCode", String.class);
+           method.setAccessible(true);
+           method.invoke(<Object>, <Parameters>);
+        } catch(NoSuchMethodException e) {
+        } catch(IllegalAccessException e) {
+        } catch(InvocationTargetException e) {
+        }
+        */
+    }
 
-/** 
-* 
-* Method: isNumeric(String str) 
-* 
-*/ 
-@Test
-public void testIsNumeric() throws Exception { 
-//TODO: Test goes here... 
-/* 
-try { 
-   Method method = VerifyChinaIDCard.getClass().getMethod("isNumeric", String.class); 
-   method.setAccessible(true); 
-   method.invoke(<Object>, <Parameters>); 
-} catch(NoSuchMethodException e) { 
-} catch(IllegalAccessException e) { 
-} catch(InvocationTargetException e) { 
-} 
-*/ 
-} 
+    /** 
+     * 
+     * Method: isNumeric(String str) 
+     * 
+     */ 
+    @Test
+    public void testIsNumeric() throws Exception { 
+        //TODO: Test goes here... 
+        /*
+        try {
+           Method method = VerifyChinaIDCard.getClass().getMethod("isNumeric", String.class);
+           method.setAccessible(true);
+           method.invoke(<Object>, <Parameters>);
+        } catch(NoSuchMethodException e) {
+        } catch(IllegalAccessException e) {
+        } catch(InvocationTargetException e) {
+        }
+        */
+    }
 
-/** 
-* 
-* Method: getAreaCode() 
-* 
-*/ 
-@Test
-public void testGetAreaCode() throws Exception { 
-//TODO: Test goes here... 
-/* 
-try { 
-   Method method = VerifyChinaIDCard.getClass().getMethod("getAreaCode"); 
-   method.setAccessible(true); 
-   method.invoke(<Object>, <Parameters>); 
-} catch(NoSuchMethodException e) { 
-} catch(IllegalAccessException e) { 
-} catch(InvocationTargetException e) { 
-} 
-*/ 
-} 
+    /** 
+     * 
+     * Method: getAreaCode() 
+     * 
+     */ 
+    @Test
+    public void testGetAreaCode() throws Exception { 
+        //TODO: Test goes here... 
+        /*
+        try {
+           Method method = VerifyChinaIDCard.getClass().getMethod("getAreaCode");
+           method.setAccessible(true);
+           method.invoke(<Object>, <Parameters>);
+        } catch(NoSuchMethodException e) {
+        } catch(IllegalAccessException e) {
+        } catch(InvocationTargetException e) {
+        }
+        */
+    }
 
-} 
+}
