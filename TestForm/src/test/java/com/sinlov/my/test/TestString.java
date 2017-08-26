5649c923eb79e04cb1c8585a451a6cface86761a
sinlov <sinlovgmppt@gmail.com>
2017-08-26 16:46:26 +0800

    add string hash code base test

---
+++ TestForm/src/test/java/com/sinlov/my/test/TestString.java
@@ -0,0 +1,23 @@
+package com.sinlov.my.test;
+
+/**
+ * <pre>
+ *     sinlov
+ *
+ *     /\__/\
+ *    /`    '\
+ *  ≈≈≈ 0  0 ≈≈≈ Hello world!
+ *    \  --  /
+ *   /        \
+ *  /          \
+ * |            |
+ *  \  ||  ||  /
+ *   \_oo__oo_/≡≡≡≡≡≡≡≡o
+ *
+ * </pre>
+ * Created by sinlov on 17/8/26.
+ */
+public class TestString {
+
+
+}
---
+++ TestForm/src/test/java/com/sinlov/my/test/TestStringTest.java
@@ -0,0 +1,35 @@
+package com.sinlov.my.test;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+/**
+ * TestString Tester.
+ *
+ * @author sinlov
+ * @version 1.0
+ * @since <pre>08/26/2017</pre>
+ */
+public class TestStringTest {
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    @Test
+    public void stringHashCode() {
+        String first = "12323";
+        System.out.println("first = " + first.hashCode());
+        String second = "\"12323\"";
+        System.out.println("second = " + second.hashCode());
+        String firstRef = first;
+        System.out.println("firstRef = " + firstRef.hashCode());
+        String firstCopy = new String(first);
+        System.out.println("firstCopy = " + firstCopy.hashCode());
+    }
+}
