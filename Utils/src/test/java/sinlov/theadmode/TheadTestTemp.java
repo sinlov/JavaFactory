c1520aeff543ca80582bd809380e154861d97282
sinlov <sinlovgmppt@gmail.com>
2018-11-15 18:08:52 +0800

    add java in go CSP mode

---
+++ Utils/src/test/java/sinlov/theadmode/TheadTestTemp.java
@@ -0,0 +1,24 @@
+package sinlov.theadmode;
+
+/**
+ * <pre>
+ *     sinlov
+ *
+ *     /\__/\
+ *    /`    '\
+ *  ≈≈≈ 0  0 ≈≈≈ Hello world!
+ *    \  --  /
+ *   /        \
+ *  /          \
+ * |            |
+ *  \  ||  ||  /
+ *   \_oo__oo_/≡≡≡≡≡≡≡≡o
+ *
+ * </pre>
+ * Created by sinlov on 2018/11/15.
+ */
+public class TheadTestTemp {
+    protected static void go(Runnable runnable) {
+        new Thread(runnable).start();
+    }
+}
